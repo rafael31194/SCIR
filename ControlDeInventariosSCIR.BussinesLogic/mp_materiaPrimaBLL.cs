3e6989fc4c8ebd55c9ab17f29c19e7c5c2d40b85
WilPolanco <polanco260593@gmail.com>
2016-09-14 07:18:15 +0930

    agregar metodo para seleccionar materias primeas- POLANCO

--- ControlDeInventariosSCIR.BussinesLogic/mp_materiaPrimaBLL.cs
+++ ControlDeInventariosSCIR.BussinesLogic/mp_materiaPrimaBLL.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using ControlDeInventariosSCIR.DataAccess;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ControlDeInventariosSCIR.BussinesLogic
 {
-    class mp_materiaPrimaBLL
+    public class mp_materiaPrimaBLL
     {
+        MateriaPrimaDAL _mpDAL = new MateriaPrimaDAL();
+
+        public DataSet SelectmpALL(int mp_id )
+        {
+            return _mpDAL.SelectMateriaPrimaAll(mp_id);
+        }
+      
+        
     }
 }
