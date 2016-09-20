8c2e9861571fe3effe2105eaa836b3e1454b3ad0
daniel morales <daniele15x@gmail.com>
2016-09-20 10:45:03 -0600

    Cambios En Gestionar Compra

---
+++ ControlDeInventariosSCIR.BussinesLogic/ComprasBLL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ControlDeInventariosSCIR.BussinessEntities;
+using ControlDeInventariosSCIR.DataAccess;
+
+namespace ControlDeInventariosSCIR.BussinesLogic
+{
+    public class ComprasBLL
+    {
+        public List<c_compra> Gestion(c_compra compra)
+        {
+            return ComprasDAL.GetDatos(compra);
+        }
+
+        public c_compra Editar(c_compra compra)
+        {
+            //   ComprasDAL compras = new ComprasDAL();
+            return ComprasDAL.GetFila(compra);
+        }
+        public c_compra GuadarEdicion(c_compra compra)
+        {
+            return ComprasDAL.Actualizar(compra);
+        }
+
+    }
+}
---
+++ ControlDeInventariosSCIR.BussinesLogic/InventarioBLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ControlDeInventariosSCIR.BussinessEntities;
+using ControlDeInventariosSCIR.DataAccess;
+
+namespace ControlDeInventariosSCIR.BussinesLogic
+{
+    public class InventarioBLL
+    {
+        public List<i_inventario> Gestion(i_inventario inv)
+        {
+            return InventarioDAL.GetDatos(inv);
+        }
+
+        public List<i_inventario> inventarioI { get; set; }
+
+    }
+
+}
